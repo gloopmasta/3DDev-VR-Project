b6437e8da3daeb01cd66dcfe70a6c0a686c8ee97
gloopmasta <steenssens.matteo@gmail.com>
2024-03-07 21:04:02 +0100

    added a simple start scene with working UI to display scores

---
+++ horror game (goodàp)/Assets/StartScene/TextHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoresText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private TextMeshProUGUI currentScoreText;
+    [SerializeField] private TextMeshProUGUI lastScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScoreText.text = "HIGH SCORE: " + PlayerPrefs.GetInt("High Score");
+        currentScoreText.text = "CURRENT SCORE: " + PlayerPrefs.GetInt("Current Score");
+        lastScoreText.text = "LAST SCORE: " + PlayerPrefs.GetInt("Last Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Configuretext()
+    {
+        
+    }
+}
